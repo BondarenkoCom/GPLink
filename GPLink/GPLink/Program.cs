b64df5bd1b844228cdf2f84e8dc93cb9ab24c07e
Артём Бондаренко <36518701+BondarenkoCom@users.noreply.github.com>
2021-05-31 23:27:04 +0300

    Add Method  PlaceNextLink
    Work Demo
    task:
    Add method For change Next Char

--- GPLink/GPLink/Program.cs
+++ GPLink/GPLink/Program.cs
@@ -13,23 +13,32 @@ namespace BruteForceYanDisk.ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Loading...");
-            //Generate URL
-            //GenerateCharURL.BruteYanUrls("https://disk.yandex.ru/d/H-mwgcDCDig7Q");
-            //Open txt
-            Console.ForegroundColor = ConsoleColor.Green;
-            //GenerateCharURL.PlaceNextLink("C:\\Users\\ABondarenko\\Desktop\\URLresult.txt");
-            //check and parse Link
-            Console.ForegroundColor = ConsoleColor.Red;
-            GenerateCharURL.DownloadStringURL("https://disk.yandex.ru/d/H-mwgcDCDig7Q");
 
+            Console.ForegroundColor = ConsoleColor.Red;
+            GenerateCharURL.BruteYanUrls("https://yadi.sk/d/AcIvY02K4FIa4w?w=1");
+            //GenerateCharURL.BruteYanUrls("https://disk.yandex.ru/d/H-mwgcDCDig7Q");
+           
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            GenerateCharURL.PlaceNextLink("C:\\Users\\ABondarenko\\Desktop\\URLresult.txt");
+            //check and parse Link
+            //GenerateCharURL.DownloadStringURL("https://disk.yandex.ru/d/H-mwgcDCDig7Q");
+            //GenerateCharURL.DownloadStringURL("https://www.google.com/");
+            
         }
     }
 
-     //https://docs.microsoft.com/ru-ru/dotnet/api/system.net.webclient?view=net-5.0 is work
+    //https://yadi.sk/d/qcIvY02K4FIa4w?w=1 correct Url
+    //https://yadi.sk/d/AcIvY02K4FIa4w?w=1 Wrong URL
+ 
+
+    //https://yadi.sk/d/qcIvY02K4FIa4w?w=1 Witch URL
+    //https://disk.yandex.ru/d/HQmwgcDCDig7Q   (Generate URL) Is not work
+
+    //https://docs.microsoft.com/ru-ru/dotnet/api/system.net.webclient?view=net-5.0 is work
     //https://disk.yandex.ru/ is work
     //https://www.lifeinvader.com/profile/sprunk not work
     //https://www.google.com/ is work
-    //https://disk.yandex.ru/d/HQmwgcDCDig7Q   (Generate URL) Is not work
 
     public static class GenerateCharURL
     {
@@ -47,6 +56,17 @@ namespace BruteForceYanDisk.ConsoleApp
                 fstream.Read(array, 0, array.Length);
                 string textFromFile = System.Text.Encoding.Default.GetString(array);
                 Console.WriteLine($"Text - {textFromFile}");
+
+                var urls = File.ReadLines(UrlPath);
+                //var urls = File.ReadLines(UrlPath).ToList();
+                //urls[3] = "https://www.google.com";
+
+
+
+                foreach (var url in urls)
+                {
+                    DownloadStringURL(url);
+                }
             }
         }
 
@@ -77,31 +97,9 @@ namespace BruteForceYanDisk.ConsoleApp
             }
             catch
             {
-
                 Console.WriteLine($"ошибка - Exception = {url}");
-                //GenerateCharURL.PlaceNextLink($"Method Work");
-
-                string UrlForPath = "C:\\Users\\ABondarenko\\Desktop\\URLresult.txt";
-
-                using (FileStream fstream = File.OpenRead($"{UrlForPath}"))
-                {
-                    byte[] array = new byte[fstream.Length];
-                    fstream.Read(array, 0, array.Length);
-                    string textFromFile = System.Text.Encoding.Default.GetString(array);
-                    Console.WriteLine($"Lenght - {textFromFile.Length}");
-                    Console.WriteLine($"Text - {textFromFile}");
-
-                    //fstream.Seek(-5, SeekOrigin.End);
-
-                    //byte[] output = new byte[4];
-                    //fstream.Read(output, 0, output.Length);
-                    // декодируем байты в строку
-                    //string textFromFile = Encoding.Default.GetString(output);
-                    //Console.WriteLine($"Length - {textFromFile.Length}");
-                    //Console.WriteLine($"Текст из файла: {textFromFile}"); // worl
-                }
+                //GenerateCharURL.PlaceNextLink($"Method Work");          
           
-                //GenerateCharURL.DownloadStringURL("https://www.google.com/");
             }
         }
 
@@ -113,7 +111,7 @@ namespace BruteForceYanDisk.ConsoleApp
         {
             string UrlYandex = YanUrl;
             string[] charForBrut = { "-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-
+           
             Console.WriteLine($"Original URL {UrlYandex}");
 
     
@@ -121,10 +119,12 @@ namespace BruteForceYanDisk.ConsoleApp
             {
                 var item = charForBrut[index];
 
-                string BruteForceString = UrlYandex.Replace("-", item);
 
+                string BruteForceString = UrlYandex.Replace("A", item);
+                //https://disk.yandex.ru/d/H-mwgcDCDig7Q
+                //string BruteForceString = UrlYandex.Replace("c", item);
                 Console.WriteLine("{0} {1}", Environment.NewLine, BruteForceString);    
-                //return BruteForceString;
+             
             }
 
             string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
@@ -134,7 +134,8 @@ namespace BruteForceYanDisk.ConsoleApp
                 {
                     var item = charForBrut[index];
 
-                    var BruteForceString = UrlYandex.Replace("-", item);
+                    var BruteForceString = UrlYandex.Replace("A", item);
+                    //string BruteForceString = UrlYandex.Replace("c", item);
                     outputFile.WriteLine("{0} {1}", Environment.NewLine, BruteForceString);
                 }
         }     
